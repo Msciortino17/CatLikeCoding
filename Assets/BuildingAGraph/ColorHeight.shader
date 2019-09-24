d3ab0f29940687b75a3847296f81798719b45e13
Micahel Sciortino <michaelsci@unity3d.com>
2019-09-24 14:31:23 -0700

    Completed the basic tutorial for building a graph. Plus initial commit for project.

---
+++ Assets/BuildingAGraph/ColorHeight.shader
@@ -0,0 +1,42 @@
+﻿Shader "Custom/ColorHeight"
+{
+    Properties
+    {
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+        _GraphRange ("GraphRange", Range(2, 8)) = 2
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Standard fullforwardshadows
+
+        #pragma target 3.0
+
+        struct Input
+        {
+			float3 worldPos;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        half _GraphRange;
+
+        UNITY_INSTANCING_BUFFER_START(Props)
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            o.Albedo.r = (IN.worldPos.x / (_GraphRange * 0.5f)) * 0.5f + 0.5f;
+            o.Albedo.g = IN.worldPos.y * 0.5f + 0.5f;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = 1;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Assets/BuildingAGraph/Graph.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate float GraphFunction(float _x);
+
+public enum Functions
+{
+    BasicSquared,
+    Sin,
+    Cos,
+    Tan,
+}
+
+public class Graph : MonoBehaviour
+{
+
+    [Header("Main Data")]
+    [SerializeField] 
+    [Range(10, 400)]
+    private int m_Resolution;
+
+    [SerializeField]
+    [Range(2, 8)]
+    private float m_Range;
+
+    private float m_ResolutionStep;
+
+    [SerializeField]
+    private bool m_Animate;
+
+    private Dictionary<Functions, GraphFunction> m_Functions;
+
+    [SerializeField]
+    private Functions m_CurrentFunction;
+    
+    [Header("References and Prefabs")]
+    [SerializeField] 
+    private GameObject m_GraphCubePrefab;
+    
+    [SerializeField] 
+    private GameObject m_GraphMarkerPrefab;
+
+    private List<Transform> m_Cubes;
+
+    /// <summary>
+    /// Called at startup
+    /// </summary>
+    void Awake()
+    {
+        m_ResolutionStep = m_Range / m_Resolution;
+        m_Cubes = new List<Transform>();
+        
+        m_Functions = new Dictionary<Functions, GraphFunction>();
+        m_Functions.Add(Functions.BasicSquared, BasicSquared);
+        m_Functions.Add(Functions.Sin, Sin);
+        m_Functions.Add(Functions.Cos, Cos);
+        m_Functions.Add(Functions.Tan, Tan);
+        
+        CreateGraphMarkers();
+        CreateGraphCubes();
+    }
+
+    /// <summary>
+    /// Standard update loop
+    /// </summary>
+    private void Update()
+    {
+        if (m_Animate)
+        {
+            UpdateCubePositions(Time.time);
+        }
+    }
+
+    /// <summary>
+    /// Create each cube we want
+    /// </summary>
+    private void CreateGraphCubes()
+    {
+        for (int i = 0; i < m_Resolution; i++)
+        {
+            float x = i * m_ResolutionStep - m_Range / 2;
+            
+            var cube = Instantiate(m_GraphCubePrefab, transform);
+            cube.transform.Translate(x, Function(x), 0.0f);
+            cube.transform.localScale = Vector3.one * m_ResolutionStep;
+            cube.GetComponent<MeshRenderer>().material.SetFloat("GraphRange", m_Range);
+            m_Cubes.Add(cube.transform);
+        }
+    }
+
+    /// <summary>
+    /// Create each graph marker
+    /// </summary>
+    private void CreateGraphMarkers()
+    {
+        int numMarkers = 100;
+        // X markers
+        for (int i = 0; i < numMarkers; i++)
+        {
+            var marker = Instantiate(m_GraphMarkerPrefab, transform);
+            marker.transform.localScale = new Vector3(0.01f, (i % 10 == 0) ? 0.2f : 0.1f, 1f);
+            marker.transform.position = new Vector3((i - numMarkers / 2) * 0.1f, 0.0f, 0.0f);
+        }
+        
+        // Y markers
+        for (int i = 0; i < numMarkers; i++)
+        {
+            var marker = Instantiate(m_GraphMarkerPrefab, transform);
+            marker.transform.localScale = new Vector3((i % 10 == 0) ? 0.2f : 0.1f, 0.01f, 1f);
+            marker.transform.position = new Vector3(0.0f, (i - numMarkers / 2) * 0.1f, 0.0f);
+        }
+    }
+
+    /// <summary>
+    /// Update each cubes position, factoring in _dT into x
+    /// </summary>
+    /// <param name="_t"></param>
+    private void UpdateCubePositions(float _t)
+    {
+        for (int i = 0; i < m_Resolution; i++)
+        {
+            Transform cube = m_Cubes[i];
+
+            float x = cube.position.x;
+            cube.position = new Vector3(x, Function(x, _t));
+        }
+    }
+
+    /// <summary>
+    /// Helper method for invoking the current graph function. Handles time.
+    /// </summary>
+    /// <param name="_x">X value</param>
+    /// <param name="_t">Time value</param>
+    /// <returns>Y value</returns>
+    private float Function(float _x, float _t = 0.0f)
+    {
+        float x = _x + _t;
+        return m_Functions[m_CurrentFunction].Invoke(x);
+    }
+
+    #region Graph Functions
+    
+    /// <summary>
+    /// Simple squared function.
+    /// </summary>
+    private float BasicSquared(float _x)
+    {
+        return _x * _x;
+    }
+
+    private float Sin(float _x)
+    {
+        return Mathf.Sin(_x);
+    }
+
+    private float Cos(float _x)
+    {
+        return Mathf.Cos(_x);
+    }
+
+    private float Tan(float _x)
+    {
+        return Mathf.Tan(_x);
+    }
+    
+    #endregion
+    
+}
